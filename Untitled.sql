ed5ffce8a01706bf62facde528bfc00090ea0ff4
mykovalenko <mykovalenko@futurica.es>
2025-10-15 17:28:00 +0200

    Delete Untitled.sql

--- Untitled.sql
+++
@@ -1,9 +0,0 @@
-select * 
-from OPENLAKE_RAW."rep_psgs"."transactions"
-where "transaction_id" > 5100000
-;
-
-select * 
-from OPENLAKE_ICE."openlakeslv"."transactions"
-where "transaction_id" > 5100000
-;
